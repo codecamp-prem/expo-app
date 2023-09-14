9a053b0c27cf21ca1d818d9462af43f5e11cbeb1
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2023-09-14 10:45:57 +0545

    some validation using prop-types

--- app/components/ImageOption/ImageOption.js
+++ app/components/ImageOption/ImageOption.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Image, View, Text } from "react-native";
+import PropTypes from "prop-types";
 
 import styles from "./styles";
 
@@ -15,4 +16,14 @@ const ImageOption = ({ image, name }) => (
     <Text style={styles.optionText}>{name}</Text>
   </View>
 );
+ImageOption.propTypes = {
+  image: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+};
+
+// default if correct type is not send: nothing is send not wrong type
+// ImageOption.propTypes = {
+//   name: "Cato predeterminada",
+// };
+
 export default ImageOption;
